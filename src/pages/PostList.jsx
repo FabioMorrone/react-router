ed2eb42f4f4677af39f488850a03dbf055b25a25
FabioMorrone <morronefabio04@gmail.com>
2025-04-02 09:41:28 +0200

    refactor PostList component structure; wrap content in a main tag for better semantics

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -4,17 +4,20 @@ export default function PostList() {
     return (
 
         <>
+            <main>
 
-            <div className="container p-4">
-                <div className="card-post card p-5">
-                    <img className="card-img-top h-75" src="https://picsum.photos/100/50" alt="Title" />
-                    <div className="card-body">
-                        <h4 className="card-title">Title</h4>
-                        <p className="card-text">BELLO TUTTO</p>
+                <div className="container p-4">
+                    <div className="card-post card p-5">
+                        <img className="card-img-top h-75" src="https://picsum.photos/100/50" alt="Title" />
+                        <div className="card-body">
+                            <h4 className="card-title">Title</h4>
+                            <p className="card-text">BELLO TUTTO</p>
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
+            </main>
 
 
 
